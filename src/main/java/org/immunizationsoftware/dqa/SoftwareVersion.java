3e66fbba982bb19b3e8d45d7e51f0e4c48817698
nathanbunker <nathanbunker@users.noreply.github.com>
2013-08-24 11:06:15 +0000

    Further improvements for testing IIS

--- src/main/java/org/immunizationsoftware/dqa/SoftwareVersion.java
+++ src/main/java/org/immunizationsoftware/dqa/SoftwareVersion.java
@@ -3,5 +3,5 @@ package org.immunizationsoftware.dqa;
 public class SoftwareVersion
 {
   public static String VERSION = "1.15";
-  public static String VERSION_FOR_SMM_DOWNLOAD = "1.13";
+  public static String VERSION_FOR_SMM_DOWNLOAD = "1.15";
 }
