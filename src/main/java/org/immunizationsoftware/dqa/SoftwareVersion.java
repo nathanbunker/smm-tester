f3ccce35ce21d50effcf467a1714ccf3a87ec562
nathanbunker <nathanbunker@users.noreply.github.com>
2015-06-22 16:56:24 +0000

    Latest 1.45 version

--- src/main/java/org/immunizationsoftware/dqa/SoftwareVersion.java
+++ src/main/java/org/immunizationsoftware/dqa/SoftwareVersion.java
@@ -3,12 +3,12 @@ package org.immunizationsoftware.dqa;
 
 public class SoftwareVersion
 {
-  public static String VERSION = "1.44";
+  public static String VERSION = "1.45";
   public static String VERSION_FOR_SMM_DOWNLOAD = "1.39";
   public static String VERSION_FOR_TESTER_DOWNLOAD = "1.39";
   
   public static String SOFTWARE_VENDOR = "OIS";
   public static String SOFTWARE_PRODUCT_NAME = "HL7 IIS Tester - Simple Message Mover";
   public static String SOFTWARE_BINARY_ID = "139001";
-  public static String SOFTWARE_RELEASE_DATE = "20150612";
+  public static String SOFTWARE_RELEASE_DATE = "20150619";
 }
