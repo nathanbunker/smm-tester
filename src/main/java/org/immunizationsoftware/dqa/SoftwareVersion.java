fc5e81019f1af7d3388917281b046b112fb870ec
nathanbunker <nathanbunker@users.noreply.github.com>
2015-07-02 13:42:31 +0000

    Minor fixes to report

--- src/main/java/org/immunizationsoftware/dqa/SoftwareVersion.java
+++ src/main/java/org/immunizationsoftware/dqa/SoftwareVersion.java
@@ -3,12 +3,12 @@ package org.immunizationsoftware.dqa;
 
 public class SoftwareVersion
 {
-  public static String VERSION = "1.46";
-  public static String VERSION_FOR_SMM_DOWNLOAD = "1.46";
-  public static String VERSION_FOR_TESTER_DOWNLOAD = "1.46";
+  public static String VERSION = "1.47";
+  public static String VERSION_FOR_SMM_DOWNLOAD = "1.47";
+  public static String VERSION_FOR_TESTER_DOWNLOAD = "1.47";
   
   public static String SOFTWARE_VENDOR = "AIRA";
   public static String SOFTWARE_PRODUCT_NAME = "HL7 IIS Tester - Simple Message Mover";
   public static String SOFTWARE_BINARY_ID = "139002";
-  public static String SOFTWARE_RELEASE_DATE = "20150701";
+  public static String SOFTWARE_RELEASE_DATE = "20150702";
 }
