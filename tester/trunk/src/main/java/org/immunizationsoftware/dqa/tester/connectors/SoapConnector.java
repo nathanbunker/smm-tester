8dc1b7c5893df72a6eee1759361f2005b4d48a78
nathanbunker <nathanbunker@68fc65ce-bdf1-4f59-8c7d-c9377a04ccf7>
2016-02-18 13:50:31 +0000

    Added support URL decoding the response

--- tester/trunk/src/main/java/org/immunizationsoftware/dqa/tester/connectors/SoapConnector.java
+++ tester/trunk/src/main/java/org/immunizationsoftware/dqa/tester/connectors/SoapConnector.java
@@ -4,6 +4,8 @@
  */
 package org.immunizationsoftware.dqa.tester.connectors;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.List;
 
 import org.immunizationsoftware.dqa.tester.connectors.tlep.Client_Service;
@@ -15,7 +17,6 @@ import org.immunizationsoftware.dqa.tester.connectors.tlep.SubmitSingleMessage;
 import org.immunizationsoftware.dqa.tester.connectors.tlep.SubmitSingleMessageRequestType;
 import org.immunizationsoftware.dqa.tester.connectors.tlep.SubmitSingleMessageResponse;
 
-
 /**
  * 
  * @author nathan
@@ -25,15 +26,13 @@ public class SoapConnector extends HttpConnector
 
   private Client_Service clientService = null;
 
-
   public SoapConnector(String label, String url) throws Exception {
     super(label, url, ConnectorFactory.TYPE_SOAP);
     clientService = new Client_ServiceStub(this.url);
   }
 
   @Override
-  public String submitMessage(String message, boolean debug) throws Exception
-  {
+  public String submitMessage(String message, boolean debug) throws Exception {
     SubmitSingleMessage submitSingleMessage = new SubmitSingleMessage();
     SubmitSingleMessageRequestType request = new SubmitSingleMessageRequestType();
     request.setFacilityID(this.facilityid);
@@ -42,14 +41,18 @@ public class SoapConnector extends HttpConnector
     request.setUsername(this.userid);
     submitSingleMessage.setSubmitSingleMessage(request);
     SubmitSingleMessageResponse response = clientService.submitSingleMessage(submitSingleMessage);
-    
-    return response.getSubmitSingleMessageResponse().get_return();
+    String responseMessage =
+
+    response.getSubmitSingleMessageResponse().get_return();
+    if (responseMessage != null && responseMessage.startsWith("MSH%7C")) {
+      responseMessage = URLDecoder.decode(responseMessage, "UTF-8");
+    }
+    return responseMessage;
 
   }
 
   @Override
-  public String connectivityTest(String message) throws Exception
-  {
+  public String connectivityTest(String message) throws Exception {
     ConnectivityTestRequestType request = new ConnectivityTestRequestType();
     request.setEchoBack(message);
     ConnectivityTest connectivityTest = new ConnectivityTest();
@@ -59,14 +62,12 @@ public class SoapConnector extends HttpConnector
   }
 
   @Override
-  protected void makeScriptAdditions(StringBuilder sb)
-  {
+  protected void makeScriptAdditions(StringBuilder sb) {
     // do nothing
   }
 
   @Override
-  protected void setupFields(List<String> fields)
-  {
+  protected void setupFields(List<String> fields) {
     super.setupFields(fields);
 
   }
