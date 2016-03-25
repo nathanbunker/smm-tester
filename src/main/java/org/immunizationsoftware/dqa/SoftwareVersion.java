f5838c4d3ca036266405f0bf549656de99272b15
nathanbunker <nathanbunker@users.noreply.github.com>
2016-03-25 11:23:26 +0000

    Updated version

--- src/main/java/org/immunizationsoftware/dqa/SoftwareVersion.java
+++ src/main/java/org/immunizationsoftware/dqa/SoftwareVersion.java
@@ -3,12 +3,12 @@ package org.immunizationsoftware.dqa;
 
 public class SoftwareVersion
 {
-  public static String VERSION = "1.63";
+  public static String VERSION = "1.64";
   public static String VERSION_FOR_SMM_DOWNLOAD = "1.63";
   public static String VERSION_FOR_TESTER_DOWNLOAD = "1.63";
   
-  public static String SOFTWARE_VENDOR = "AIRA";
+  public static String SOFTWARE_VENDOR = "OIS";
   public static String SOFTWARE_PRODUCT_NAME = "IIS Tester - Simple Message Mover";
-  public static String SOFTWARE_BINARY_ID = "139016";
-  public static String SOFTWARE_RELEASE_DATE = "20160318";
+  public static String SOFTWARE_BINARY_ID = "139017";
+  public static String SOFTWARE_RELEASE_DATE = "20160325";
 }
