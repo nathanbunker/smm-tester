b1a2fb59b29e28ffc9dab9202950be0942ae44e4
nathanbunker <nathanbunker@68fc65ce-bdf1-4f59-8c7d-c9377a04ccf7>
2016-09-09 11:50:24 +0000

    Made updates to message modifier service

--- tester/trunk/src/main/java/org/immunizationsoftware/dqa/SoftwareVersion.java
+++ tester/trunk/src/main/java/org/immunizationsoftware/dqa/SoftwareVersion.java
@@ -3,12 +3,12 @@ package org.immunizationsoftware.dqa;
 
 public class SoftwareVersion
 {
-  public static String VERSION = "1.70";
+  public static String VERSION = "1.71";
   public static String VERSION_FOR_SMM_DOWNLOAD = "1.68";
   public static String VERSION_FOR_TESTER_DOWNLOAD = "1.69";
   
   public static String SOFTWARE_VENDOR = "OIS";
   public static String SOFTWARE_PRODUCT_NAME = "IIS Tester - Simple Message Mover";
-  public static String SOFTWARE_BINARY_ID = "139022";
-  public static String SOFTWARE_RELEASE_DATE = "20160830";
+  public static String SOFTWARE_BINARY_ID = "139023";
+  public static String SOFTWARE_RELEASE_DATE = "20160909";
 }
