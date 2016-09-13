fe7056859d044777203498ba0dc441fd9b3ef341
nathanbunker <nathanbunker@users.noreply.github.com>
2016-09-13 16:57:52 +0000

    Added overdue code for reading MCIR results

--- src/main/java/org/immunizationsoftware/dqa/SoftwareVersion.java
+++ src/main/java/org/immunizationsoftware/dqa/SoftwareVersion.java
@@ -3,12 +3,12 @@ package org.immunizationsoftware.dqa;
 
 public class SoftwareVersion
 {
-  public static String VERSION = "1.72";
+  public static String VERSION = "1.73";
   public static String VERSION_FOR_SMM_DOWNLOAD = "1.68";
   public static String VERSION_FOR_TESTER_DOWNLOAD = "1.69";
   
   public static String SOFTWARE_VENDOR = "OIS";
   public static String SOFTWARE_PRODUCT_NAME = "IIS Tester - Simple Message Mover";
-  public static String SOFTWARE_BINARY_ID = "139023";
-  public static String SOFTWARE_RELEASE_DATE = "20160909";
+  public static String SOFTWARE_BINARY_ID = "139024";
+  public static String SOFTWARE_RELEASE_DATE = "20160913";
 }
