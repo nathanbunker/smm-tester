a0a36e09b84cb149f6a95fc410b519ee335a46b6
Nathan Bunker <nbunker@immregistries.org>
2020-08-04 11:12:37 -0600

    AART-943 Added new procedure for adding funding source and funding elgibility

--- src/main/java/org/immregistries/smm/transform/procedure/ProcedureFactory.java
+++ src/main/java/org/immregistries/smm/transform/procedure/ProcedureFactory.java
@@ -6,7 +6,16 @@ public class ProcedureFactory {
   public static final String REMOVE_VACCINATION_GROUPS = "REMOVE_VACCINATION_GROUPS";
   public static final String ADD_FUNDING_ELGIBILITY_TO_ALL_RXA =
       "ADD_FUNDING_ELGIBILITY_TO_ALL_RXA";
-  public static final String ADD_FUNDING_SOURCE_TO_ALL_RXA = "ADD_FUNDING_SOURCE_TO_ALL_RXA";
+
+  public static final String ADD_OBX_FOR_FUNDING_ELIGIBILITY_TO_ALL_RXA =
+      "ADD_OBX_FOR_FUNDING_ELIGIBILITY_TO_ALL_RXA";
+  public static final String ADD_OBX_FOR_FUNDING_ELIGIBILITY_TO_ALL_ADMINISTERED_RXA =
+      "ADD_OBX_FOR_FUNDING_ELIGIBILITY_TO_ALL_ADMINISTERED_RXA";
+  public static final String ADD_OBX_FOR_FUNDING_SOURCE_TO_ALL_RXA =
+      "ADD_OBX_FOR_FUNDING_SOURCE_TO_ALL_RXA";
+  public static final String ADD_OBX_FOR_FUNDING_SOURCE_TO_ALL_ADMINISTERED_RXA =
+      "ADD_OBX_FOR_FUNDING_SOURCE_TO_ALL_ADMINISTERED_RXA";
+
   public static final String ANONYMIZE_AND_UPDATE_RECORD = "ANONYMIZE_AND_UPDATE_RECORD";
 
   public static ProcedureInterface getProcedure(String procedureName, Transformer transformer) {
@@ -15,8 +24,19 @@ public class ProcedureFactory {
       procedureInterface = new RemoveVaccinationGroupsProcedure();
     } else if (procedureName.equalsIgnoreCase(ADD_FUNDING_ELGIBILITY_TO_ALL_RXA)) {
       procedureInterface = new AddFundingEligibilityToAllRxa();
-    } else if (procedureName.equalsIgnoreCase(ADD_FUNDING_SOURCE_TO_ALL_RXA)) {
-      procedureInterface = new AddFundingSourceToAllRxa();
+    } else if (procedureName.equalsIgnoreCase(ADD_OBX_FOR_FUNDING_ELIGIBILITY_TO_ALL_RXA)) {
+      procedureInterface =
+          new AddFundingToRxa(AddFundingToRxa.Type.ELIGIBILITY, AddFundingToRxa.VaccinationGroups.ALL);
+    } else if (procedureName.equalsIgnoreCase(ADD_OBX_FOR_FUNDING_SOURCE_TO_ALL_RXA)) {
+      procedureInterface =
+          new AddFundingToRxa(AddFundingToRxa.Type.SOURCE, AddFundingToRxa.VaccinationGroups.ALL);
+    } else if (procedureName.equalsIgnoreCase(ADD_OBX_FOR_FUNDING_SOURCE_TO_ALL_ADMINISTERED_RXA)) {
+      procedureInterface = new AddFundingToRxa(AddFundingToRxa.Type.SOURCE,
+          AddFundingToRxa.VaccinationGroups.ADMINISTERED_ONLY);
+    } else if (procedureName
+        .equalsIgnoreCase(ADD_OBX_FOR_FUNDING_ELIGIBILITY_TO_ALL_ADMINISTERED_RXA)) {
+      procedureInterface = new AddFundingToRxa(AddFundingToRxa.Type.ELIGIBILITY,
+          AddFundingToRxa.VaccinationGroups.ADMINISTERED_ONLY);
     } else if (procedureName.equalsIgnoreCase(ANONYMIZE_AND_UPDATE_RECORD)) {
       procedureInterface = new AnonymizeAndUpdateRecord();
     }
